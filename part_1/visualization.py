c1ef15bbc32e4fbd6189cde4a93d33d22fefe8f3
KevinUrrutia <kurru006@ucr.edu>
2022-04-26 10:28:57 -0700

    started part 1

---
+++ part_1/visualization.py
@@ -0,0 +1,18 @@
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def visualization():
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    plt.show()
+
+
+
+def main():
+    visualization()
+
+if __name__== "__main__":
+    main()
